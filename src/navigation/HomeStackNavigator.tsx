a1b7719b21b85e2d52eca6d5040f03d58a4b95bf
naoya9411 <sn7sn8@gmail.com>
2022-01-27 00:13:15 +0900

    ユーザ情報登録画面作成

--- src/navigation/HomeStackNavigator.tsx
+++ src/navigation/HomeStackNavigator.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SignInScreen } from '../screens/SignInScreen';
+import { RegistUserInfoScreen } from '../screens/RegistUserInfoScreen';
 import { DrawerNavigator } from './DrawerNavigator';
 
 const RootStack = createStackNavigator();
@@ -10,6 +11,10 @@ export const RootStackScreen = () => {
     <RootStack.Navigator screenOptions={{ headerShown: false }}>
       <RootStack.Group>
         <RootStack.Screen name="SignIn" component={SignInScreen} />
+        <RootStack.Screen
+          name="RegistUserInfo"
+          component={RegistUserInfoScreen}
+        />
         <RootStack.Screen name="Main" component={DrawerNavigator} />
       </RootStack.Group>
     </RootStack.Navigator>
