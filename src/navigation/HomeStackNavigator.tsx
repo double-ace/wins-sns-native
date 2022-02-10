96af7e43a18e154d28e2b3cecf7baf493f6fb124
naoya9411 <sn7sn8@gmail.com>
2022-02-11 01:14:08 +0900

    ログイン処理修正

--- src/navigation/HomeStackNavigator.tsx
+++ src/navigation/HomeStackNavigator.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SignInScreen } from '../screens/SignInScreen';
+import { SignUpScreen } from '../screens/SignUpScreen';
 import { RegistUserInfoScreen } from '../screens/RegistUserInfoScreen';
 import { DrawerNavigator } from './DrawerNavigator';
+import { getData, setAccess } from '../scripts/asyncStore';
+import { requestHttpGet } from '../scripts/requestBase';
 
 const RootStack = createStackNavigator();
 
 export const RootStackScreen = () => {
+  // const [access, setAccess] = useState('');
+  // const [profile, setProfile] = useState('');
+  // useEffect(() => {
+  //   const sample = async () => {
+  //     console.log('test');
+  //     const aa = await getData('access');
+  //     const bb = await requestHttpGet('/api/v1/user/profile');
+  //     aa && setAccess(aa);
+  //     bb && setProfile(bb.data);
+  //     console.log(profile);
+  //   };
+
+  //   sample();
+  // }, []);
+
   return (
     <RootStack.Navigator screenOptions={{ headerShown: false }}>
       <RootStack.Group>
         <RootStack.Screen name="SignIn" component={SignInScreen} />
+        <RootStack.Screen name="SignUp" component={SignUpScreen} />
         <RootStack.Screen
           name="RegistUserInfo"
           component={RegistUserInfoScreen}
--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,23 +1,46 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import { SafeAreaView, TextInput, View, Text, StyleSheet } from 'react-native';
-import { Button } from 'native-base';
+import React, { useEffect, useState } from 'react';
+import { TouchableWithoutFeedback, Keyboard, SafeAreaView, TextInput, View, Text, StyleSheet } from 'react-native';
+import { Button, Link } from 'native-base';
+import { Loading } from '../components/Loading';
 import { authLogin } from '../scripts/requestAuth';
-import { setData, getData } from '../scripts/asyncStore';
+import { setData, getData, delData } from '../scripts/asyncStore';
+import { requestHttpGet } from '../scripts/requestBase';
 
 export const SignInScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [invalid, setInvalid] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    try {
+      initApp();
+    } catch (e) {
+      console.log(e);
+      setIsLoading(false);
+    }
+    return console.log('endSignIn');
+  }, []);
+
+  const initApp = async () => {
+    console.log('=============================');
+    const access = await getData('access');
+    if (access) {
+      await checkProfile();
+    } else {
+      console.log('access: ', access);
+      setIsLoading(false);
+    }
+  };
 
   const login = async () => {
     const param = { email, password };
     try {
       const res = await authLogin({ email, password });
       if (res) {
-        invalid === false ? setInvalid(false) : null;
-        navigation.navigate('Main');
+        await checkProfile();
       } else {
+        console.log('responce is faild');
         setInvalid(true);
       }
     } catch (e) {
@@ -25,42 +48,62 @@ export const SignInScreen = ({ navigation }) => {
       alert(e);
     }
     // 開発用
-    console.log('error');
-    // navigation.navigate('Main');
-    navigation.navigate('RegistUserInfo');
+    console.log('login error');
+  };
+
+  const checkProfile = async () => {
+    const res = await requestHttpGet('/api/v1/user/profile/');
+    if (res.data.length) {
+    // プロフィール情報が取得できた場合はメイン画面へ、取得できない場合はユーザ情報登録画面へ遷移
+    console.log('profile: ', res);
+    res.data.length
+      ? navigation.navigate('Main')
+      : navigation.navigate('RegistUserInfo');
+    }
+    await delData('access')
+    setIsLoading(false);
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.inner}>
-        {invalid ? (
-          <Text style={styles.invalidText}>
-            メールアドレスまたはパスワードが正しくありません
-          </Text>
-        ) : null}
-        <TextInput
-          style={styles.input}
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-          autoCapitalize="none"
-          keyboardType="email-address"
-          textContentType="emailAddress"
-          placeholder="Email Address"
-        />
-        <TextInput
-          style={styles.input}
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          autoCapitalize="none"
-          textContentType="password"
-          placeholder="Password"
-          secureTextEntry
-        />
-        <Button onPress={login} bg="#00EB7D" _text={{ color: '#fff' }}>
-          ログイン
-        </Button>
-      </View>
-    </SafeAreaView>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.inner}>
+            {invalid ? (
+              <Text style={styles.invalidText}>
+                メールアドレスまたはパスワードが正しくありません
+              </Text>
+            ) : null}
+            <TextInput
+              style={styles.input}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              textContentType="emailAddress"
+              placeholder="メールアドレス"
+            />
+            <TextInput
+              style={styles.input}
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              autoCapitalize="none"
+              textContentType="password"
+              placeholder="パスワード"
+              secureTextEntry
+            />
+            <Button onPress={login} my="2" bg="#00EB7D" _text={{ color: '#fff' }}>
+              ログイン
+            </Button>
+            <Link alignSelf="flex-end" onPress={() => navigation.navigate('SignUp')}>アカウントをお持ちでない方</Link>
+          </View>
+        </SafeAreaView>
+        </TouchableWithoutFeedback>
+      )}
+    </>
   );
 };
 
