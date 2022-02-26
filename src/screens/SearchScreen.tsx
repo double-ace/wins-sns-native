e4a04d7aca24fb0fe1a76cf0aca7f848e7397611
naoya9411 <sn7sn8@gmail.com>
2022-02-26 14:53:50 +0900

    検索機能実装

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -7,45 +7,68 @@ import {
   Text,
   ListRenderItemInfo,
 } from 'react-native';
-import { Link, Avatar, Button, Input, Box, HStack, Spacer } from 'native-base';
-import { postData } from '../../assets/postData.json';
+import {
+  Link,
+  Avatar,
+  Button,
+  Input,
+  Box,
+  HStack,
+  Spacer,
+  Center,
+} from 'native-base';
 import { getData } from '../scripts/asyncStore';
-import axios, { AxiosResponse } from 'axios';
-import { requestHttpGet } from '../scripts/requestBase';
+import { requestHttpGet, requestHttpPost } from '../scripts/requestBase';
+import { SkeletonItem } from '../components/SkeletonUserItem';
 
-type PostData = {
-  id: string | number;
-  poster: string;
-  date: Date | string;
-  content: string;
+type UserList = {
+  id: string;
+  nickname: string;
+  imageUrl: string;
 };
 
-const posts: PostData[] = postData;
-
 export const SearchScreen = ({ navigation }) => {
-  const [access, setAccess] = useState('');
-  const [shop, setShop] = useState<string[]>([]);
-  const [user, setUser] = useState([])
+  const [userList, setUserList] = useState<UserList[]>([]);
+  const [isSearching, setIsSearching] = useState(false);
 
   useEffect(() => {
-    // const sample = async () => {
-    //   const aa = await getData('access');
-    //   aa ? setAccess(aa) : null;
-    // };
-    // sample();
-  });
+    getUserList();
+  }, []);
 
-  const getUsers = async () => {
-    // ユーザ取得
-    await requestHttpGet('/api/v1/user//')
-  }
+  // ユーザ取得
+  const getUserList = async () => {
+    const res = await requestHttpGet('/api/v1/sns/users/');
+    setUserList(res.data);
+  };
 
-  const onChangeValue = (value) => {
+  const onChangeKeyword = async (keyword: string) => {
+    setIsSearching(true);
     // 入力値でユーザをフィルター
+    console.log('keyword: ', keyword);
+    if (keyword) {
+      const res = await requestHttpGet(`/api/v1/sns/users/?keyword=${keyword}`);
+      setUserList(res.data);
+    }
+    setIsSearching(false);
+  };
 
-  }
+  // 友達申請
+  const handleReq = async (id: string) => {
+    // const res = await requestHttpPost
+    const param = {
+      req_to: id,
+    };
+    const res = await requestHttpPost(
+      '/api/v1/sns/friend-request/',
+      param,
+      true
+    );
+    setUserList((pre) => [
+      ...pre.filter((item) => item.id !== res.data.req_to),
+    ]);
+  };
 
-  const renderItem = ({ item }: ListRenderItemInfo<PostData>) => {
+  const renderItem = ({ item }: ListRenderItemInfo<UserList>) => {
     return (
       <Box py="3" style={styles.postContainer} key={item.id}>
         <HStack alignItems="center">
@@ -53,10 +76,18 @@ export const SearchScreen = ({ navigation }) => {
             <Avatar size="md"></Avatar>
           </Link>
           <View style={styles.postHeaderTxtContainer}>
-            <Text style={styles.posterName}>{item.poster}</Text>
+            <Text style={styles.posterName}>{item.nickname}</Text>
           </View>
           <Spacer />
-          <Button px="6" h="10" mr="2" colorScheme="error" variant="outline">取消</Button>
+          <Button
+            px="6"
+            h="10"
+            bg="emerald.400"
+            _text={{ fontWeight: 'bold' }}
+            onPress={() => handleReq(item.id)}
+          >
+            友達申請
+          </Button>
         </HStack>
       </Box>
     );
@@ -64,8 +95,20 @@ export const SearchScreen = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Input variant="rounded" mb="2" mx="1" placeholder='ユーザ名入力' />
-      <FlatList data={posts} renderItem={renderItem} scrollEnabled />
+      <Input
+        variant="rounded"
+        mb="2"
+        mx="1"
+        placeholder="ユーザ名入力"
+        onChangeText={(value) => onChangeKeyword(value)}
+      />
+      {isSearching ? (
+        <SkeletonItem />
+      ) : !userList.length ? (
+        <Center>一致するユーザが見つかりません</Center>
+      ) : (
+        <FlatList data={userList} renderItem={renderItem} scrollEnabled />
+      )}
     </SafeAreaView>
   );
 };
@@ -80,8 +123,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     paddingHorizontal: 12,
     paddingVertical: 8,
-    marginBottom: 4,
-    borderWidth: 1,
+    borderBottomWidth: 1,
     borderColor: '#E9EAEB',
   },
   avatar: {
