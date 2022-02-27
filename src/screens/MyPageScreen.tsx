7f8c2ce49885710a0f096e35e90c0bcd5cf5f97b
naoya9411 <sn7sn8@gmail.com>
2022-02-27 11:18:41 +0900

    名前表示・変更機能

--- src/screens/MyPageScreen.tsx
+++ src/screens/MyPageScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { SafeAreaView, View, FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
+import { SafeAreaView, View, FlatList, TextInput } from 'react-native';
 import {
   Link,
   Avatar,
@@ -11,11 +11,16 @@ import {
   Switch,
   Spacer,
   HStack,
+  Center,
+  Modal,
+  Input,
+  FormControl,
 } from 'native-base';
 import { AntDesign, Ionicons, Feather } from '@expo/vector-icons';
 import { postData } from '../../assets/postData.json';
 import { delData } from '../scripts/asyncStore';
-import { requestHttpGet } from '../scripts/requestBase';
+import { requestHttpGet, requestHttpPatch } from '../scripts/requestBase';
+import { previousDay } from 'date-fns';
 
 type PostData = {
   id: string;
@@ -24,24 +29,37 @@ type PostData = {
   content: string;
 };
 
-const posts: PostData[] = postData;
+type Nickname = {
+  id: string | number;
+  nickname: string;
+};
+
+const posts: any[] = postData;
 
 export const MyPageScreen = ({ navigation }) => {
-  const logout = async () => {
-    try {
-      await delData('access');
-      navigation.navigate('SignIn');
-    } catch (e) {
-      console.log('logoutError========');
-      console.log(e);
-    }
-  };
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [newName, setNewName] = useState('');
+  const [nickname, setNickname] = useState<Nickname>({ id: '', nickname: '' });
+
+  useEffect(() => {
+    getMyProfile();
+  }, []);
 
   const getMyProfile = async () => {
     // ユーザ名とプロフ画像取得
-    const proRes = await requestHttpGet('/api/v1/user/profile');
+    const nameRes = await requestHttpGet('/api/v1/sns/nickname/');
+    setNickname({ id: nameRes.data[0].id, nickname: nameRes.data[0].nickname });
     // 友達一覧取得
-    const friendRes = await requestHttpGet('');
+    // const friendRes = await requestHttpGet('');
+  };
+
+  const handleSaveName = async () => {
+    // nickname変更処理
+    const res = await requestHttpPatch(`/api/v1/sns/nickname/${nickname.id}/`, {
+      nickname: newName,
+    });
+    res.result && setNickname((pre) => ({ ...pre, nickname: newName }));
+    setShowEditModal(false);
   };
 
   const renderItem = ({ item }) => {
@@ -83,8 +101,41 @@ export const MyPageScreen = ({ navigation }) => {
       </Box>
     );
   };
+
+  const NameEditModal = () => {
+    return (
+      <Modal isOpen={showEditModal}>
+        <Modal.Content maxWidth="400px">
+          <Modal.Body>
+            <Center h={16}>
+              <Input
+                style={{ width: '100%' }}
+                onChangeText={(value) => setNewName(value)}
+              />
+            </Center>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="ghost"
+                colorScheme="blueGray"
+                onPress={() => setShowEditModal(false)}
+              >
+                キャンセル
+              </Button>
+              <Button bg="green.400" onPress={handleSaveName}>
+                変更
+              </Button>
+            </Button.Group>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
+    );
+  };
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#f1f5f9' }}>
+      <NameEditModal />
       <Box p={2} position="relative">
         <Pressable
           position="absolute"
@@ -99,20 +150,27 @@ export const MyPageScreen = ({ navigation }) => {
         <HStack alignItems="center" mb={2}>
           <Link position="relative" onPress={() => alert('Works!')}>
             <Avatar size="xl"></Avatar>
-            <Box position="absolute" bottom="1" right="0">
-              <Feather name="edit" size={18} color="gray" />
-            </Box>
+            <Center
+              bg="blueGray.700"
+              p={1}
+              borderRadius={50}
+              position="absolute"
+              bottom="1"
+              right="0"
+            >
+              <Feather name="camera" size={18} color="#d4d4d4" />
+            </Center>
           </Link>
           <Box ml={2}>
             <Stack flexDirection="row" alignItems="center">
-              <Box>
+              <Pressable onPress={() => setShowEditModal(true)}>
                 <Text fontSize="3xl">
-                  my name
-                  <Feather name="edit" size={15} color="gray" />
+                  {nickname.nickname}
+                  <Feather name="edit-2" size={15} color="gray" />
                 </Text>
-              </Box>
+              </Pressable>
             </Stack>
-            <Box flexDirection="row" justifyContent="space-between">
+            <Box flexDirection="row" justifyContent="space-between" ml={2}>
               <Pressable
                 alignItems="center"
                 onPress={() => navigation.navigate('Friends')}
@@ -120,17 +178,23 @@ export const MyPageScreen = ({ navigation }) => {
                 <Text>友達</Text>
                 <Text>10</Text>
               </Pressable>
-              <Button variant="outline" px="8" ml="4">
-                来店通知をOFF
-              </Button>
             </Box>
           </Box>
         </HStack>
-        <View>
-          <Button onPress={logout}>ログアウト</Button>
-        </View>
       </Box>
       <View>
+        <Box
+          m={2}
+          mb={1}
+          _text={{
+            fontSize: 16,
+            fontWeight: 'bold',
+            letterSpacing: 1,
+            color: 'emerald.500',
+          }}
+        >
+          本日来店した友達
+        </Box>
         <FlatList
           data={posts}
           renderItem={renderItem}
