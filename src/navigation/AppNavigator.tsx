4a7aecf68d709a3aa30533ca21bfbc9135a701e3
naoya9411 <sn7sn8@gmail.com>
2022-05-18 00:42:43 +0900

    nativebaseのwarning出ないようにする対応

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootStackScreen } from './HomeStackNavigator';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['NativeBase:']);
 
 export const AppNavigator = () => {
   return (
