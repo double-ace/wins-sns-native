cafd46e07bff743830bc6934d1c81fd01f44a896
naoya9411 <sn7sn8@gmail.com>
2022-05-17 23:19:29 +0900

    入力チェック処理追加

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -9,98 +9,79 @@ import {
   StyleSheet,
 } from 'react-native';
 import { Button, Link } from 'native-base';
-import { authLogin } from '../scripts/requestAuth';
-import { setData, getData } from '../scripts/asyncStore';
 import { createAccount } from '../scripts/requestAuth';
-import Constants from 'expo-constants';
-import * as Notifications from 'expo-notifications';
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
-
-const regPushNotifications = async () => {
-  let token;
-  if (Constants.isDevice) {
-    const { status } = await Notifications.getPermissionsAsync();
-    if (status !== 'granted') {
-      // 許可がない場合
-      alert('Failed to get push token for push notification');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log(token);
-  } else {
-  }
+const ERR_MSG = {
+  empEmail: 'メールアドレスを入力してください',
+  empPW: 'パスワードを入力してください',
+  empConfirmPW: 'パスワード(確認用)を入力してください',
+  sameEmail: '既にこのメールアドレスは使われています',
+  invalidEmail: 'メールアドレスの形式が正しくありません',
+  pwTooShort: '8文字以上の半角英数字で設定してください',
+  pwNoMatch: 'パスワードとパスワード(確認用)が一致しません',
 };
 
 export const SignUpScreen = ({ navigation }) => {
-  const [pushToken, setPushToken] = useState(null);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPw, setConfirmPw] = useState('');
-  const [validList, setValidList] = useState({
-    common: {
-      value: 'アカウント登録に失敗しました',
-      isValid: false,
-    },
-    sendEmail: {
-      value: '既にこのメールアドレスは使われています',
-      isValid: false,
-    },
-    inputEmail: {
-      value: 'メールの形式が正しくありません',
-      isValid: false,
-    },
-    password: {
-      value: '8文字以上の半角英数字で設定してください',
-      isValid: false,
-    },
-    confirmPw: {
-      value: 'パスワードとパスワード(確認用)が一致しません',
-      isValid: false,
-    },
+  const [validation, setValidation] = useState({
+    valid: false,
+    msg: '',
   });
 
   const signUp = async () => {
-    if (password === confirmPw) {
-      const param = { email, password };
-      try {
-        const res = await createAccount({ email, password });
-        if (res) {
-          navigation.reset({ index: 0, routes: [{ name: 'RegistUserInfo' }] });
-        } else {
-          const clone = Object.assign({}, validList);
-          clone.common.isValid = true;
-          setValidList({ ...clone });
-        }
-      } catch (e) {
-        const clone = Object.assign({}, validList);
-        clone.common.isValid = true;
-        setValidList({ ...clone });
-        alert(e);
-      }
-    } else {
+    // 入力チェック
+    if (email === '') {
+      setValidation({ valid: true, msg: ERR_MSG.empEmail });
+      return;
+    } else if (password === '') {
+      setValidation({ valid: true, msg: ERR_MSG.empPW });
+      return;
+    } else if (confirmPw === '') {
+      setValidation({ valid: true, msg: ERR_MSG.empConfirmPW });
+      return;
     }
 
-    // 開発用
-    console.log('error');
-    // navigation.navigate('Main');
-    navigation.navigate('RegistUserInfo');
+    // メールアドレスの形式チェック
+    const regex =
+      /^[a-zA-Z0-9_+-]+(\.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+    if (!regex.test(email)) {
+      setValidation({ valid: true, msg: ERR_MSG.invalidEmail });
+      return;
+    }
+
+    // パスワード一致チェック
+    if (password !== confirmPw) {
+      setValidation({ valid: true, msg: ERR_MSG.pwNoMatch });
+      return;
+    }
+
+    const res = await createAccount({ email, password });
+    if (res) {
+      navigation.reset({ index: 0, routes: [{ name: 'RegistUserInfo' }] });
+    } else {
+      setValidation({ valid: true, msg: ERR_MSG.sameEmail });
+    }
+  };
+
+  const onChangePw = (value: string) => {
+    const regex = /^[0-9a-zA-Z]*$/;
+    // パスワード文字数&半角英数字チェック
+    if (!regex.test(value) || password.length < 8) {
+      setValidation({ valid: true, msg: ERR_MSG.pwTooShort });
+    } else {
+      setValidation({ valid: false, msg: '' });
+    }
+    setPassword(value);
   };
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <SafeAreaView style={styles.container}>
         <View style={styles.inner}>
-          {validList.common.isValid ? (
-            <Text style={styles.invalidText}>
-              メールアドレスまたはパスワードが正しくありません
-            </Text>
+          {validation.valid ? (
+            <Text style={styles.invalidText}>{validation.msg}</Text>
           ) : null}
           <TextInput
             style={styles.input}
@@ -114,7 +95,7 @@ export const SignUpScreen = ({ navigation }) => {
           <TextInput
             style={styles.input}
             value={password}
-            onChangeText={(text) => setPassword(text)}
+            onChangeText={(text) => onChangePw(text)}
             autoCapitalize="none"
             textContentType="password"
             placeholder="パスワード"
@@ -140,6 +121,7 @@ export const SignUpScreen = ({ navigation }) => {
           </Button>
           <Link
             alignSelf="flex-end"
+            _text={{ color: 'primary.600' }}
             onPress={() =>
               navigation.reset({
                 index: 0,
