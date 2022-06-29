890366c17662a6219b82097794975ea43c7dec96
naoya9411 <sn7sn8@gmail.com>
2022-06-29 18:06:49 +0900

    fix

--- src/scripts/date.ts
+++ src/scripts/date.ts
@@ -4,9 +4,9 @@ export const formatDate = (date: string) => {
   const now = new Date();
   const target = new Date(date);
   if (format(target, 'yyyy/MM/dd') === format(now, 'yyyy/MM/dd')) {
-    return format(target, 'HH:mm');
+    return format(target, 'H:mm');
   }
   if (getYear(target) === getYear(now)) {
-    return format(target, 'MM/dd HH:mm');
+    return format(target, 'M/d H:mm');
   }
 };
